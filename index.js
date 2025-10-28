928ff08b3ff7bc040288f40e73504fe354c1cb0f
mahliyoA <mahliyoA>
2025-10-28 16:34:01 +0500

    bot tugatildi

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ const TOKEN = "8360980250:AAHsOvqAZSLVY8XZgqNIXZgKzJLlmEs5bMM"
 //bot yaratildi//
 const bot = new TelegramBot(TOKEN, {polling:true})
 
-bot.on("message", function (msg) {
-    bot.sendMessage(msg.chat.id, "Hello")
-    console.log("Salom");  
+bot.on("message", function (msg){
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    const firstname = msg.chat.first_name
+
+ bot.sendMessage(chatId, 'Xush kelibsiz, ${firstname)',{
+    reply_markup: {
+        keyboard: [
+        [{ text: "Boshlash"}],
+        [{ text: "Menu"}, { text: "sozlamalar"}]
+        ],
+        resize_keyboard:true,
+    }
+ })
+
+    console.log(msg);
+    
 })
+console.log("bot ishga tushdi");
