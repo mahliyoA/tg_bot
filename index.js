1d75ec810405f29d143d0b8253a65690e9b6f899
Makhliyo <azimovamahliyo>
2025-11-01 16:21:48 +0500

    tayyor

--- index.js
+++ index.js
@@ -3,24 +3,45 @@ import TelegramBot from "node-telegram-bot-api";
 const TOKEN = "8360980250:AAHsOvqAZSLVY8XZgqNIXZgKzJLlmEs5bMM"
 
 //bot yaratildi//
-const bot = new TelegramBot(TOKEN, {polling:true})
+const bot = new TelegramBot(TOKEN, { polling: true })
 
-bot.on("message", function (msg){
+
+bot.on("message", async function (msg) {
     const chatId = msg.chat.id;
     const text = msg.text;
-    const firstname = msg.chat.first_name
+    const firstname = msg.chat.first_name;
+    const menuPhoto = "./rasm.jpg";
 
- bot.sendMessage(chatId, `Xush kelibsiz, ${firstname}`,{
-    reply_markup: {
-        keyboard: [
-        [{ text: "Boshlash"}],
-        [{ text: "Menu"}, { text: "sozlamalar"}]
-        ],
-        resize_keyboard:true,
+    if (text == "/start") {
+        bot.sendMessage(chatId, `Xush kelibsiz, ${firstname}`, {
+            reply_markup: {
+                keyboard: [
+                    [{ text: "Boshlash" }],
+                    [{ text: "Menu" }, { text: "Sozlamalar" }],
+                ],
+                resize_keyboard: true,
+            },
+        });
+    } else if (text == "Boshlash") {
+        bot.sendMessage(chatId, "Salom, sizga qanday yordam bera olaman?");
+    } else if (text == "Menu") {
+        const kutingXabari = await bot.sendMessage(chatId, "Iltimos kuting...");
+
+        setTimeout(function () {
+            bot.deleteMessage(chatId, kutingXabari.message_id);
+            bot.sendPhoto(chatId, menuPhoto, {
+                caption: "Bizning menyuyimiz...",
+                reply_markup: {
+                    keyboard: [
+                        [{ text: "Manti" }, { text: "Karam" }],
+                        [{ text: "Shashlik" }, { text: "Hotdog" }],
+                    ],
+                },
+            });
+        }, 1000);
     }
- })
 
     console.log(msg);
-    
-})
-console.log("bot ishga tushdi");
+});
+
+console.log("Bot ishga tushdi...");
